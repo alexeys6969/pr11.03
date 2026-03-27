f26b06cf89e4d417f5036500b5f31c194a8524d5
alexeys6969 <alexshashin.2023@gmail.com>
2026-03-27 14:45:03 +0500

    Разработанные переменные. Шашин

--- app/src/main/java/com/example/pr11/MainActivity.java
+++ app/src/main/java/com/example/pr11/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.pr11;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,4 +29,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    LocationListener _LocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(location == null) return;
+            else {
+                String message = "";
+                if(location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+                    message += "\n Местоположение определено с помощью GPS: долгота - " + location.getLatitude() + " широта - " + location.getLongitude();
+                }
+                if(location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
+                    message += "\n Местоположение определено с помощью интернета: долгота - " + location.getLatitude() + " широта - " + location.getLongitude();
+                }
+                result.setText(message);
+            }
+        }
+    };
 }
